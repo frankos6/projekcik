f4b55f11d68b7b965e6372bd9d3730da70415b55
frankos6 <frankos6@wp.pl>
2023-05-25 19:38:37 +0200

    removed city list

--- app/app3/Search.tsx
+++ app/app3/Search.tsx
@@ -15,11 +15,12 @@ const Search = (props: Props) => {
     const [search, setSearch] = React.useState('');
     const handleClick = () => {
         if (search === "") return;
-        router.push(`/app3/search?q=${search}`);
+        router.push(`/app3/weather?q=${search}`);
+        router.refresh();
     }
     return (
         <Stack spacing={2} direction="row" justifyContent="center">
-            <TextField variant="outlined" label="Search for a city" value={search} onChange={(e)=>setSearch(e.target.value.trim())} fullWidth/>
+            <TextField variant="outlined" label="Search for a city" value={search} onChange={(e)=>setSearch(e.target.value.trimStart())} fullWidth/>
             <Button variant="contained" onClick={handleClick} endIcon={<SearchIcon />}>Search</Button>
         </Stack>
     );
--- app/app3/weather/page.tsx
+++ app/app3/weather/page.tsx
@@ -1,28 +1,46 @@
 // @flow
 'use client';
 import * as React from 'react';
-import {notFound, useSearchParams} from "next/navigation";
+import {notFound, useRouter, useSearchParams} from "next/navigation";
 import {useEffect, useState} from "react";
-import {getWeather, Weather} from "@/lib/openweather";
+import {findCity, Geo, getWeather, Weather} from "@/lib/openweather";
 import ArrowUpward from "@mui/icons-material/ArrowUpward";
 
 type Props = {
 
 };
 const Page = (props: Props) => {
+    const router = useRouter();
+    const regionNames = new Intl.DisplayNames(
+        ['en'], {type: 'region'}
+    );
     const searchParams = useSearchParams();
-    const lon = searchParams.get('lon');
-    const lat = searchParams.get('lat');
+    const q = searchParams.get('q');
     const [data,setData] = useState<Weather>();
-    if (!lon || !lat) notFound();
+    const [city,setCity] = useState<Geo>();
+    const [isLoading, setLoading] = useState(false);
+    if (!q) notFound();
 
     useEffect(()=>{
-        getWeather(+lon,+lat)
-            .then(data=>setData(data))
+        setLoading(true);
+        findCity(q??"")
+            .then(d=> {
+                setCity(d[0]);
+                return getWeather(d[0].lon, d[0].lat)
+            })
+            .then(d=>setData(d))
+            .then(()=>setLoading(false))
     },[])
+
+    if (isLoading) return <div className='container-fluid w-100 d-flex flex-column align-items-center justify-content-center h-100'>
+        <div className='spinner-border mt-5' style={{height: '6rem', width: '6rem', borderWidth: '.4rem'}} role='status'>
+        </div>
+        <p className='h2 mt-4'>Loading...</p>
+    </div>
 
     return (
         <div>
+            <h2>{city?.name}, {city?.state ? `${city.state},` : ''} {city?.country ? regionNames.of(city.country) : ""}</h2>
             {data?.weather[0].main} <br/>
             {data?.main.temp} C <br />
             {data?.wind.speed} km/h
